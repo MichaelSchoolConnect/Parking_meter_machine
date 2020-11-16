a1f67d6dd12ca8e700b301c1104af4a2d3aca158
27678 <michaelmay149@gmail.com>
2020-11-16 23:17:12 +0200

    A test class to test the MainActivity

---
+++ app/src/androidTest/java/com/example/parkingmetermachine/TestMainActivity.java
@@ -0,0 +1,36 @@
+package com.example.parkingmetermachine;
+
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.filters.LargeTest;
+
+import org.junit.Rule;
+import org.junit.runner.RunWith;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class TestMainActivity {
+
+    @Rule
+    public ActivityScenarioRule<MainActivity> activityActivityScenarioRule =
+            new ActivityScenarioRule<>(MainActivity.class);
+
+    @org.junit.Test
+    public void listGoesOverTheFold() {
+        onView(withText("Enter amount:")).check(matches(isDisplayed()));
+        onView(withId(R.id.e_amount)).perform(typeText("100"));
+        onView(withId(R.id.e_cost)).perform(typeText("35"));
+        onView(withId(R.id.button)).perform(click());
+    }
+
+
+
+}
